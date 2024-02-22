2742a6e535ebe04fbdec5ecd1425e3210ee77bf9
Daniel Matilla <danimatilla@outlook.es>
2024-02-22 23:16:16 +0100

    [FEAT] change sample view

--- feature/sample/lib/sample.screen.dart
+++ feature/sample/lib/sample.screen.dart
@@ -12,23 +12,87 @@ class SampleScreen extends StatelessWidget {
 
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
+          alignment: Alignment.center,
+          fit: StackFit.expand,
           children: [
-            if (state.sample != null) Text(state.sample!.answer),
-            FilledButton(
-              onPressed: () => cubit.request('yes'),
-              child: const Text('Request YES'),
+            if (state.sample != null)
+              Image.network(
+                state.sample!.urlGif,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  return loadingProgress == null
+                      ? child
+                      : Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes!,
+                          ),
+                        );
+                },
+              ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                if (state.sample != null) _Text(state: state),
+                _Buttons(cubit: cubit),
+              ],
             ),
-            FilledButton(
-              onPressed: () => cubit.request('maybe'),
-              child: const Text('Request MAYBE'),
-            ),
-            FilledButton(
-              onPressed: () => cubit.request(null),
-              child: const Text('Request'),
-            )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Buttons extends StatelessWidget {
+  const _Buttons({required this.cubit});
+  final SampleCubit cubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom: 16,
+        left: 16,
+        right: 16,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          FilledButton(
+            onPressed: () => cubit.request('yes'),
+            child: const Text('YES'),
+          ),
+          FilledButton(
+            onPressed: () => cubit.request(null),
+            child: const Text('?'),
+          ),
+          FilledButton(
+            onPressed: () => cubit.request('maybe'),
+            child: const Text('MAYBE'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Text extends StatelessWidget {
+  const _Text({required this.state});
+  final SampleState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Text(
+          state.sample!.answer,
+          style: const TextStyle(
+            fontSize: 124,
+            fontWeight: FontWeight.bold,
+            color: Colors.white24,
+          ),
         ),
       ),
     );
